aac9a3933851997d0eb1e22a121530284617a3bf
AndyLCQ <lecoqandy@gmail.com>
2015-07-25 10:31:06 +0200

    Réinitialisation propre du porjet

--- kernel/js/liens.js
+++
@@ -1,9 +0,0 @@
-function desafficher(divID)
-{
-	document.getElementById(divID).style.display="none";
-}
-function afficher(divID)
-{
-	desafficher(divID);
-	document.getElementById(divID).style.display="visible";
-}
