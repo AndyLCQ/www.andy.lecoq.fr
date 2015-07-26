caf7dd1fbf57586d9da0bd3702466941e1d91650
AndyLCQ <lecoqandy@gmail.com>
2015-07-26 19:13:16 +0200

    Modification lien index.php

---
+++ index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<title>Andy LECOQ | </title>
+		<meta name="description" content="Site personnel de Andy LECOQ, développeur informatique."/>
+		<meta name="author" content="Andy LECOQ"/>
+		<meta name="copyright" content="Andy LECOQ. Copyright (c) 2015"/>
+		
+                
+                <link href="kernel/css/foundation.css" rel="stylesheet" type="text/css">
+                <link href="kernel/css/normalize.css" rel="stylesheet" type="text/css">
+                <link href="kernel/css/app.css" rel="stylesheet" type="text/css">
+                
+	</head>
+	<body>
+            <header class="header">
+                <div class="row">
+                    <div class="small-12 medium-12 large-12 columns">
+                        <h1>Andy LECOQ<span class="hidden-for-small">En recherche d'emploi</span></h1>
+                    </div>
+                </div>
+            </header>
+            <div id="content">
+                <div class="columns">
+                    <img class="image-principale" src="kernel/img/angers.jpeg" title="image principale" alt="Angers" />
+                    <label class="image">Angers (Juin 2015)</label>
+                </div>
+                   
+
+                
+                
+            </div>
+            <footer class="footer">
+                <div id="medias">
+                    <div class="row">
+                        <div class="medium-6 large-6 columns">
+                            <h6>Derniers tweets</h6>
+                        </div>
+                        <div class="medium-6 large-6 columns">
+                            <h6>Liens utiles</h6>
+                            <ul class="no-bullet">
+                                <li><i class="fi-mail"></i> <a href="old.cv/cv_index.php">Mon curriculum vitae</a></li>
+                                <li><i class="fi-mail"></i> <a href="mailto:lecoqandy@gmail.com" target="_top">M'écrire</a></li>
+                                <li><i class="fi-social-facebook"></i><a href="https://www.facebook.com/andy.nplify">Facebook</a></li>
+				<li><i class="fi-social-twitter"></i><a href="https://twitter.com/Andy_lcq"> Twitter</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div id="copyright">
+                    <div class="row">
+                        <div class="small-12 medium-12 large-12 columns">
+                            Andy LECOQ &copy; 2015
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <script src="kernel/js/analyticstracking.js"></script>
+            <script src="kernel/js/vendor/jquery.js"></script>
+            <script src="kernel/js/vendor/jquery.cookie.js"></script>
+            <script src="kernel/js/vendor/modernizr.js"></script>
+            <script src="kernel/js/foundation.min.js"></script>
+            <script type="text/javascript">
+                $(document).foundation();
+            </script>
+	</body>
+</html>
+ 
